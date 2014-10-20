8bb84ce344e8fa55d9969390f3ea40ce6124505a
daniel rodriguez <maieutiquer@gmail.com>
2014-10-20 21:06:16 +0300

    finish tests and class for hero

--- hero.py
+++ hero.py
@@ -4,9 +4,31 @@ class Hero:
         self.name = name
         self.health = health
         self.nickname = nickname
+        self.__MAX_HEALTH = health
 
     def known_as(self):
         return "{} the {}".format(self.name, self.nickname)
 
     def get_health(self):
         return self.health
+
+    def is_alive(self):
+        return self.health > 0
+
+    def take_damage(self, damage_points):
+        if self.health > damage_points:
+            self.health -= damage_points
+        else:
+            self.health = 0
+
+    def take_healing(self, healing_points):
+
+        if not self.is_alive():
+            return False
+
+        if self.health + healing_points < self.__MAX_HEALTH:
+            self.health += healing_points
+        else:
+            self.health = self.__MAX_HEALTH
+
+        return True
--- test_hero.py
+++ test_hero.py
@@ -19,6 +19,44 @@ class TestHero(unittest.TestCase):
         self.bron_hero.health = 80
         self.assertEqual(80, self.bron_hero.get_health())
 
+    def test_is_alive(self):
+        self.bron_hero.health = 80
+        self.assertTrue(self.bron_hero.is_alive())
+
+    def test_is_dead(self):
+        self.bron_hero.health = 0
+        self.assertFalse(self.bron_hero.is_alive())
+
+    def test_take_damage_survive(self):
+        self.bron_hero.health = 100
+        self.bron_hero.take_damage(30)
+        self.assertEqual(70, self.bron_hero.get_health())
+        self.assertTrue(self.bron_hero.is_alive())
+
+    def test_take_damage_die(self):
+        self.bron_hero.health = 100
+        self.bron_hero.take_damage(110)
+        self.assertEqual(0, self.bron_hero.get_health())
+        self.assertFalse(self.bron_hero.is_alive())
+
+    def test_take_healing_regular(self):
+        self.bron_hero.health = 40
+        self.bron_hero.take_healing(20)
+        self.assertEqual(60, self.bron_hero.get_health())
+        self.assertTrue(self.bron_hero.is_alive())
+
+    def test_take_healing_overheal(self):
+        self.bron_hero.health = 90
+        self.assertTrue(self.bron_hero.take_healing(20))
+        self.assertEqual(100, self.bron_hero.get_health())
+        self.assertTrue(self.bron_hero.is_alive())
+
+    def test_take_healing_dead(self):
+        self.bron_hero.health = 0
+        self.assertFalse(self.bron_hero.take_healing(20))
+        self.assertEqual(0, self.bron_hero.get_health())
+        self.assertFalse(self.bron_hero.is_alive())
+
 
 if __name__ == '__main__':
     unittest.main()
