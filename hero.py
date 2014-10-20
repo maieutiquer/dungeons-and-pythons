7f6ddb374611293124c73b6a0491e0015868922b
daniel rodriguez <maieutiquer@gmail.com>
2014-10-20 20:11:18 +0300

    insert useless comment

--- hero.py
+++ hero.py
@@ -2,3 +2,11 @@ class Hero:
 
     def __init__(self, name, health, nickname):
         self.name = name
+        self.health = health
+        self.nickname = nickname
+
+    def known_as(self):
+        return "{} the {}".format(self.name, self.nickname)
+
+    def get_health(self):
+        return self.health
--- test_hero.py
+++ test_hero.py
@@ -5,11 +5,19 @@ from hero import Hero
 class TestHero(unittest.TestCase):
 
     def setUp(self):
-        self.new_hero = Hero("Bron", 100, "Dragonslayer")
+        self.bron_hero = Hero("Bron", 100, "Dragonslayer")
 
-    def test_attr(self):
-        self.assertEqual("Bron", self.new_hero.name)
-        self.assertEqual(health, self.new_hero.name)
+    def test_attributes(self):
+        self.assertEqual("Bron", self.bron_hero.name)
+        self.assertEqual(100, self.bron_hero.health)
+        self.assertEqual("Dragonslayer", self.bron_hero.nickname)
+
+    def test_known_as(self):
+        self.assertEqual("Bron the Dragonslayer", self.bron_hero.known_as())
+
+    def test_get_health(self):
+        self.bron_hero.health = 80
+        self.assertEqual(80, self.bron_hero.get_health())
 
 
 if __name__ == '__main__':
